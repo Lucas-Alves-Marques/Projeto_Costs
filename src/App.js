4e86f91baa55f01c6a6bd2105eafd69fd29395c5
Lucas Alves Marques <lucas.marquesalv24@gmail.com>
2025-02-01 14:07:02 -0300

    Adição da NavBar e do Footer

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import Home from './Components/Pages/Home.js';
 import Company from './Components/Pages/Company.js';
 import Contact from './Components/Pages/Contact.js';
 import NewProject from './Components/Pages/NewProject.js';
-
 import Conteiner from "./Components/Layout/Conteiner.js";
+import NavBar from "./Components/Layout/NavBar.js";
+import Footer from "./Components/Layout/Footer.js";
+import Projects from "./Components/Pages/Projects.js";
+
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to={'/'}>Inicio</Link>
-        <Link to={'/company'}>Empresa</Link>
-        <Link to={'/contact'}>Contatos</Link>
-        <Link to={'/newproject'}>Novos Projetos</Link>
-      </ul>
+      <NavBar />
       <Conteiner customClass='min-height'>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/Projects" element={<Projects />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Conteiner>
-      <footer>Footer</footer>
+      <Footer />
     </Router>
   );
 }
